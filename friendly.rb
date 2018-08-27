fef50e10b1f86b981da64877564daabd38a4fea2
shubhachaudhary <shubha.learning@gmail.com>
2018-08-27 16:45:03 +1000

    changes

--- friendly.rb
+++ friendly.rb
@@ -9,26 +9,50 @@ member.each do |k,v|
     break else  puts"user name and password not correct .would like to quit"
     end 
 end
-b = gets.chop
-#/while b != "quit"   
+b = gets.chomp
+sum =0 
+while true 
+  print " > "
+ #/b= gets.chomp
  if b == "balance"
-    puts "your current balance is 0 .Would like to deposit,Enter No want to exit"
-    i = 0
-    blc=Array.new
-    y=gets.chomp
-    while y!="No" 
-      blc << gets.chomp
-      y=blc.last 
-    end 
-     blc.pop     
+  puts "your current balance is 0 .Would like to deposit,Enter No want to exit"
+  i = 0
+  blc=Array.new
+  y=gets.chomp
+  #counter = 0
+
+  while y!="No" 
+    blc << y.to_i
+    print " > "
+    y = gets.chomp
+  end 
+
     p blc
-end   
- if b == "withdraw"
-   puts" how much money would you like to withdraw"
-     with =gets.chomp.to_i
-   if blc.last <= with
+    sum=0
+    blc.each do |bal|
+     sum= sum + bal
+    end
+  #puts " What would you like to? \n> "
+   
+elsif b == "withdraw"
+ puts" how much money would you like to withdraw"
+   with =gets.chomp.to_i
+
+  if sum >= with
         puts "please take the money"
-        blc.last = blc.last - with
-    end   
-  end
+        sum = sum - with
+  else
+      puts ":( Insufficient !!!!"
+  end   
+elsif b == "deposit"
+  puts" how much would you like to deposit"
+  d=gets.chomp.to_i
+  sum =sum + d
+  #puts "your current balance is #{sum}"
+
+end
+puts "your current balance is #{sum} .Would like to deposit,Enter No want to exit > "
+b= gets.chomp
+break if ["quit"].include?(b)
+end  
  
